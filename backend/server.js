0f3d887f34a384003932a7660c4628887edcc194
Vivek Kumar Singh <rcfvivek@gmail.com>
2024-03-25 15:38:03 +0530

    Added path bug fix

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const dotenv = require("dotenv");
 const connectDB = require("./config/db.js");
 const menuRoutes = require('./routes/menuRoutes.js');
